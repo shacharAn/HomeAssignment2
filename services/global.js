41c1ee19afae63272b7042d585a8f0d65d67f1f1
shachar anafi <shachar3231@gmail.com>
2025-05-10 18:01:02 +0300

    להמשיך דארק מוד חוץ מדף הבית

--- services/global.js
+++ services/global.js
@@ -3,20 +3,23 @@ darkModeToggle.addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
 
     if (document.body.classList.contains('dark-mode')) {
-        localStorage.setItem('dark-mode', 'enabled');
-        darkModeToggle.innerHTML = '<i class="fas fa-sun"></i>'; // אייקון שמש
+        localStorage.setItem('dark-mode','enabled');
+        darkModeToggle.innerHTML =  '<i class="fas fa-sun"></i>';  
     } else {
-        localStorage.setItem('dark-mode', 'disabled');
-        darkModeToggle.innerHTML = '<i class="fas fa-moon"></i>'; // אייקון ירח
+        localStorage.setItem('dark-mode','disabled');
+        darkModeToggle.innerHTML = '<i class="fas fa-moon"></i>'; 
     }
 });
 
 if (localStorage.getItem('dark-mode') === 'enabled') {
     document.body.classList.add('dark-mode');
-    darkModeToggle.innerHTML = '<i class="fas fa-sun"></i>'; // אייקון שמש
+    darkModeToggle.innerHTML = '<i class="fas fa-sun"></i>'; 
+}else{
+    darkModeToggle.innerHTML = '<i class="fas fa-moon"></i>'; 
 };
-// Toggle the mobile menu on clicking the hamburger icon
-document.getElementById('icon').addEventListener('click', function() {
+
+
+document.getElementById('hamburger-icon').addEventListener('click', function() {
     const menuList = document.getElementById('menu-list');
-    menuList.classList.toggle('show');  // Toggle the menu visibility
+    menuList.classList.toggle('show');  
 });
